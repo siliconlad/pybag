1543a288efddeb5310dca5f5a4f66d911f413ae3
Angus Stewart <angus.stewart@sensmore.ai>
2025-07-14 19:54:27 +0000

    fix: Tidy

--- tests/test_cdr.py
+++ tests/test_cdr.py
@@ -1,45 +1,56 @@
-import sys
-import unittest
-from pathlib import Path
-
-sys.path.insert(0, str(Path(__file__).resolve().parents[1] / "src"))
-
+import pytest
 from pybag.encoding.cdr import CdrDecoder, CdrEncoder
 
 
-class TestCdrEncoderDecoder(unittest.TestCase):
-    def test_encode_decode_all_types(self) -> None:
-        values = [
-            ("bool", True),
-            ("int8", -8),
-            ("uint8", 200),
-            ("int16", -12345),
-            ("uint16", 54321),
-            ("int32", -12345678),
-            ("uint32", 12345678),
-            ("int64", -1234567890123456789),
-            ("uint64", 9876543210987654321),
-            ("float32", 3.1415926),
-            ("float64", 2.718281828459045),
-            ("string", "hello world"),
-        ]
+@pytest.mark.parametrize('little_endian', [True, False])
+def test_encode_decode_all_primitive_types(little_endian: bool) -> None:
+    data_values = [
+        ('bool', [True, False]),
+        ('int8', [-8, 8]),
+        ('uint8', [0, 200]),
+        ('int16', [-12_345, 12_345]),
+        ('uint16', [0, 54_321]),
+        ('int32', [-12_345_678, 12_345_678]),
+        ('uint32', [0, 12_345_678]),
+        ('int64', [-12_345_678_901, 12_345_678_901]),
+        ('uint64', [0, 9_876_543_210]),
+        ('float32', [-0.5, 0.5]),                    # Exact in float32
+        ('float64', [0.0009765625, -0.0009765625]),  # Exact in float64
+        ('string', ['', 'hello world']),
+    ]
 
-        encoder = CdrEncoder(little_endian=True)
-        for type_name, value in values:
-            encoder.encode(type_name, value)
+    # Encode the data
+    encoder = CdrEncoder(little_endian=little_endian)
+    for type_name, values in data_values:
+        for v in values:
+            encoder.encode(type_name, v)
 
-        data = encoder.save()
+    data = encoder.save()
 
-        decoder = CdrDecoder(data)
-
-        for type_name, value in values:
-            decoded = decoder.parse(type_name)
-            if isinstance(value, float):
-                places = 6 if type_name == "float32" else 15
-                self.assertAlmostEqual(decoded, value, places=places)
-            else:
-                self.assertEqual(decoded, value)
+    # Decode the data
+    decoder = CdrDecoder(data)
+    for type_name, values in data_values:
+        for v in values:
+            assert decoder.parse(type_name) == v
 
 
-if __name__ == "__main__":
-    unittest.main()
+@pytest.mark.parametrize('little_endian', [True, False])
+def test_encode_decode_array(little_endian: bool) -> None:
+    # Encode the data
+    encoder = CdrEncoder(little_endian=little_endian)
+    encoder.array('int32', [1, 2, 3])
+
+    # Decode the data
+    decoder = CdrDecoder(encoder.save())
+    assert decoder.array('int32', 3) == [1, 2, 3]
+
+
+@pytest.mark.parametrize('little_endian', [True, False])
+def test_encode_decode_sequence(little_endian: bool) -> None:
+    # Encode the data
+    encoder = CdrEncoder(little_endian=little_endian)
+    encoder.sequence('int32', [1, 2, 3])
+
+    # Decode the data
+    decoder = CdrDecoder(encoder.save())
+    assert decoder.sequence('int32') == [1, 2, 3]
