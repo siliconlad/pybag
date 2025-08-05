9d5515d5a916e15f3388ed4694a677f3498aeb6c
Angus Stewart <angus.stewart@sensmore.ai>
2025-08-05 22:11:11 +0000

    fix: Tidy

--- tests/schema/test_ros2msg.py
+++ tests/schema/test_ros2msg.py
@@ -22,27 +22,13 @@ def test_parse_primitive_field():
     )
     ros2_schema, sub_schemas = Ros2MsgSchema().parse(schema)
 
+    assert ros2_schema.name == "pkg/msg/Primitive"
+    assert len(ros2_schema.fields) == 1
     assert "my_int" in ros2_schema.fields
     field = ros2_schema.fields["my_int"]
     assert isinstance(field, Primitive)
     assert field.type == "int32"
     assert sub_schemas == {}
-
-
-def test_parse_static_array_field():
-    schema_text = "int32[5] values\n"
-    schema = SchemaRecord(
-        id=1,
-        name="pkg/msg/StaticArray",
-        encoding="ros2msg",
-        data=schema_text.encode("utf-8"),
-    )
-    ros2_schema, _ = Ros2MsgSchema().parse(schema)
-
-    field = ros2_schema.fields["values"]
-    assert isinstance(field, Array)
-    assert field.type == "int32"
-    assert field.length == 5
 
 
 def test_parse_unbounded_sequence_field():
@@ -55,9 +41,51 @@ def test_parse_unbounded_sequence_field():
     )
     ros2_schema, _ = Ros2MsgSchema().parse(schema)
 
+    assert ros2_schema.name == "pkg/msg/SeqArray"
+    assert len(ros2_schema.fields) == 1
+    assert "values" in ros2_schema.fields
     field = ros2_schema.fields["values"]
     assert isinstance(field, Sequence)
     assert field.type == "int32"
+
+
+def test_parse_bounded_array():
+    schema_text = "int32[<=5] limited\n"
+    schema = SchemaRecord(
+        id=1,
+        name="pkg/msg/BoundedArray",
+        encoding="ros2msg",
+        data=schema_text.encode("utf-8"),
+    )
+    ros2_schema, _ = Ros2MsgSchema().parse(schema)
+
+    assert ros2_schema.name == "pkg/msg/BoundedArray"
+    assert len(ros2_schema.fields) == 1
+    assert "limited" in ros2_schema.fields
+    field = ros2_schema.fields["limited"]
+    assert isinstance(field, Array)
+    assert field.type == "int32"
+    assert field.length == 5
+    # TODO: Check the length limit
+
+
+def test_parse_static_array_field():
+    schema_text = "int32[5] values\n"
+    schema = SchemaRecord(
+        id=1,
+        name="pkg/msg/StaticArray",
+        encoding="ros2msg",
+        data=schema_text.encode("utf-8"),
+    )
+    ros2_schema, _ = Ros2MsgSchema().parse(schema)
+
+    assert ros2_schema.name == "pkg/msg/StaticArray"
+    assert len(ros2_schema.fields) == 1
+    assert "values" in ros2_schema.fields
+    field = ros2_schema.fields["values"]
+    assert isinstance(field, Array)
+    assert field.type == "int32"
+    assert field.length == 5
 
 
 def test_parse_complex_header_field():
@@ -70,24 +98,12 @@ def test_parse_complex_header_field():
     )
     ros2_schema, _ = Ros2MsgSchema().parse(schema)
 
+    assert ros2_schema.name == "pkg/msg/HasHeader"
+    assert len(ros2_schema.fields) == 1
+    assert "header" in ros2_schema.fields
     field = ros2_schema.fields["header"]
     assert isinstance(field, Complex)
     assert field.type == "std_msgs/Header"
-
-
-def test_parse_complex_same_package_field():
-    schema_text = "Sub other\n"
-    schema = SchemaRecord(
-        id=1,
-        name="pkg/msg/Main",
-        encoding="ros2msg",
-        data=schema_text.encode("utf-8"),
-    )
-    ros2_schema, _ = Ros2MsgSchema().parse(schema)
-
-    field = ros2_schema.fields["other"]
-    assert isinstance(field, Complex)
-    assert field.type == "pkg/Sub"
 
 
 def test_parse_constant_field():
@@ -100,6 +116,9 @@ def test_parse_constant_field():
     )
     ros2_schema, _ = Ros2MsgSchema().parse(schema)
 
+    assert ros2_schema.name == "pkg/msg/Const"
+    assert len(ros2_schema.fields) == 1
+    assert "X" in ros2_schema.fields
     field = ros2_schema.fields["X"]
     assert isinstance(field, Constant)
     assert field.type == "int32"
@@ -116,9 +135,185 @@ def test_parse_string_with_length_limit():
     )
     ros2_schema, _ = Ros2MsgSchema().parse(schema)
 
+    assert ros2_schema.name == "pkg/msg/BoundedString"
+    assert len(ros2_schema.fields) == 1
+    assert "short_name" in ros2_schema.fields
     field = ros2_schema.fields["short_name"]
     assert isinstance(field, Primitive)
     assert field.type == "string"
+    # TODO: Check the length limit
+
+
+def test_parse_default_integer_value():
+    schema_text = "int32 count 5\n"
+    schema = SchemaRecord(
+        id=1,
+        name="pkg/msg/DefaultValue",
+        encoding="ros2msg",
+        data=schema_text.encode("utf-8"),
+    )
+    ros2_schema, _ = Ros2MsgSchema().parse(schema)
+
+    assert ros2_schema.name == "pkg/msg/DefaultValue"
+    assert len(ros2_schema.fields) == 1
+    assert "count" in ros2_schema.fields
+    field = ros2_schema.fields["count"]
+    assert isinstance(field, Primitive)
+    assert field.type == "int32"
+    # TODO: Check the default value
+
+
+def test_parse_bounded_string_array():
+    schema_text = "string<=10[<=5] names\n"
+    schema = SchemaRecord(
+        id=1,
+        name="pkg/msg/BoundedStringArray",
+        encoding="ros2msg",
+        data=schema_text.encode("utf-8"),
+    )
+    ros2_schema, _ = Ros2MsgSchema().parse(schema)
+
+    assert ros2_schema.name == "pkg/msg/BoundedStringArray"
+    assert len(ros2_schema.fields) == 1
+    assert "names" in ros2_schema.fields
+    field = ros2_schema.fields["names"]
+    assert isinstance(field, Array)
+    assert field.type == "string"
+    assert field.length == 5
+    # TODO: Check the length limit of the string
+
+
+def test_parse_default_string_value_double_quotes():
+    schema_text = 'string name "John"\n'
+    schema = SchemaRecord(
+        id=1,
+        name="pkg/msg/StringDefaultDouble",
+        encoding="ros2msg",
+        data=schema_text.encode("utf-8"),
+    )
+    ros2_schema, _ = Ros2MsgSchema().parse(schema)
+
+    assert ros2_schema.name == "pkg/msg/StringDefaultDouble"
+    assert len(ros2_schema.fields) == 1
+    assert "name" in ros2_schema.fields
+    field = ros2_schema.fields["name"]
+    assert isinstance(field, Primitive)
+    assert field.type == "string"
+    # TODO: Check the default value
+
+
+def test_parse_default_string_value_single_quotes():
+    schema_text = "string nickname 'Johnny'\n"
+    schema = SchemaRecord(
+        id=1,
+        name="pkg/msg/StringDefaultSingle",
+        encoding="ros2msg",
+        data=schema_text.encode("utf-8"),
+    )
+    ros2_schema, _ = Ros2MsgSchema().parse(schema)
+
+    assert ros2_schema.name == "pkg/msg/StringDefaultSingle"
+    assert len(ros2_schema.fields) == 1
+    assert "nickname" in ros2_schema.fields
+    field = ros2_schema.fields["nickname"]
+    assert isinstance(field, Primitive)
+    assert field.type == "string"
+    # TODO: Check the default value
+
+
+def test_parse_default_string_value_with_hash_double_quotes():
+    schema_text = 'string tag "hello#world"\n'
+    schema = SchemaRecord(
+        id=1,
+        name="pkg/msg/StringWithHash",
+        encoding="ros2msg",
+        data=schema_text.encode("utf-8"),
+    )
+    ros2_schema, _ = Ros2MsgSchema().parse(schema)
+
+    assert ros2_schema.name == "pkg/msg/StringWithHash"
+    assert len(ros2_schema.fields) == 1
+    assert "tag" in ros2_schema.fields
+    field = ros2_schema.fields["tag"]
+    assert isinstance(field, Primitive)
+    assert field.type == "string"
+    # TODO: Check the default value
+
+
+def test_parse_default_string_value_with_hash_single_quotes():
+    schema_text = 'string tag \'hello#world\'\n'
+    schema = SchemaRecord(
+        id=1,
+        name="pkg/msg/StringWithHash",
+        encoding="ros2msg",
+        data=schema_text.encode("utf-8"),
+    )
+    ros2_schema, _ = Ros2MsgSchema().parse(schema)
+
+    assert ros2_schema.name == "pkg/msg/StringWithHash"
+    assert len(ros2_schema.fields) == 1
+    assert "tag" in ros2_schema.fields
+    field = ros2_schema.fields["tag"]
+    assert isinstance(field, Primitive)
+    assert field.type == "string"
+    # TODO: Check the default value
+
+
+def test_parse_default_array_of_ints():
+    schema_text = "int32[] samples [-200, -100, 0, 100, 200]\n"
+    schema = SchemaRecord(
+        id=1,
+        name="pkg/msg/ArrayDefault",
+        encoding="ros2msg",
+        data=schema_text.encode("utf-8"),
+    )
+    ros2_schema, _ = Ros2MsgSchema().parse(schema)
+
+    assert ros2_schema.name == "pkg/msg/ArrayDefault"
+    assert len(ros2_schema.fields) == 1
+    assert "samples" in ros2_schema.fields
+    field = ros2_schema.fields["samples"]
+    assert isinstance(field, Sequence)
+    assert field.type == "int32"
+    # TODO: Check the default value
+
+
+def test_parse_constant_string_field_double_quotes():
+    schema_text = 'string GREETING="hello#world"\n'
+    schema = SchemaRecord(
+        id=1,
+        name="pkg/msg/StringConstWithHash",
+        encoding="ros2msg",
+        data=schema_text.encode("utf-8"),
+    )
+    ros2_schema, _ = Ros2MsgSchema().parse(schema)
+
+    assert ros2_schema.name == "pkg/msg/StringConstWithHash"
+    assert len(ros2_schema.fields) == 1
+    assert "GREETING" in ros2_schema.fields
+    field = ros2_schema.fields["GREETING"]
+    assert isinstance(field, Constant)
+    assert field.type == "string"
+    assert field.value == "hello#world"
+
+
+def test_parse_constant_string_field_single_quotes():
+    schema_text = "string GREETING='hello#world'\n"
+    schema = SchemaRecord(
+        id=1,
+        name="pkg/msg/StringConstWithHash",
+        encoding="ros2msg",
+        data=schema_text.encode("utf-8"),
+    )
+    ros2_schema, _ = Ros2MsgSchema().parse(schema)
+
+    assert ros2_schema.name == "pkg/msg/StringConstWithHash"
+    assert len(ros2_schema.fields) == 1
+    assert "GREETING" in ros2_schema.fields
+    field = ros2_schema.fields["GREETING"]
+    assert isinstance(field, Constant)
+    assert field.type == "string"
+    assert field.value == "hello#world"
 
 
 def test_parse_sub_message_schema():
@@ -135,171 +330,22 @@ def test_parse_sub_message_schema():
     )
     ros2_schema, sub_schemas = Ros2MsgSchema().parse(schema)
 
+    assert ros2_schema.name == "pkg/msg/WithPoint"
+    assert len(ros2_schema.fields) == 1
+    assert "point" in ros2_schema.fields
     field = ros2_schema.fields["point"]
     assert isinstance(field, Complex)
     assert field.type == "geometry_msgs/Point"
+
+    assert len(sub_schemas) == 1
     assert "geometry_msgs/Point" in sub_schemas
     point_schema = sub_schemas["geometry_msgs/Point"]
     assert isinstance(point_schema.fields["x"], Primitive)
+    assert point_schema.fields["x"].type == "float64"
     assert isinstance(point_schema.fields["y"], Primitive)
+    assert point_schema.fields["y"].type == "float64"
     assert isinstance(point_schema.fields["z"], Primitive)
-
-
-def test_invalid_field_name_raises_error():
-    schema_text = "int32 invalid__name\n"
-    schema = SchemaRecord(
-        id=1,
-        name="pkg/msg/InvalidName",
-        encoding="ros2msg",
-        data=schema_text.encode("utf-8"),
-    )
-    parser = Ros2MsgSchema()
-    with pytest.raises(Ros2MsgError):
-        parser.parse(schema)
-
-
-def test_parse_bounded_array():
-    schema_text = "int32[<=5] limited\n"
-    schema = SchemaRecord(
-        id=1,
-        name="pkg/msg/BoundedArray",
-        encoding="ros2msg",
-        data=schema_text.encode("utf-8"),
-    )
-    ros2_schema, _ = Ros2MsgSchema().parse(schema)
-
-    field = ros2_schema.fields["limited"]
-    assert isinstance(field, Array)
-    assert field.type == "int32"
-    assert field.length == 5
-
-
-def test_parse_default_integer_value():
-    schema_text = "int32 count 5\n"
-    schema = SchemaRecord(
-        id=1,
-        name="pkg/msg/DefaultValue",
-        encoding="ros2msg",
-        data=schema_text.encode("utf-8"),
-    )
-    ros2_schema, _ = Ros2MsgSchema().parse(schema)
-
-    field = ros2_schema.fields["count"]
-    assert isinstance(field, Primitive)
-    assert field.type == "int32"
-    assert field.default == 5
-
-
-def test_parse_bounded_string_array():
-    schema_text = "string<=10[<=5] names\n"
-    schema = SchemaRecord(
-        id=1,
-        name="pkg/msg/BoundedStringArray",
-        encoding="ros2msg",
-        data=schema_text.encode("utf-8"),
-    )
-    ros2_schema, _ = Ros2MsgSchema().parse(schema)
-
-    field = ros2_schema.fields["names"]
-    assert isinstance(field, Array)
-    assert field.type == "string"
-    assert field.length == 5
-
-
-def test_parse_default_string_value_double_quotes():
-    schema_text = 'string name "John"\n'
-    schema = SchemaRecord(
-        id=1,
-        name="pkg/msg/StringDefaultDouble",
-        encoding="ros2msg",
-        data=schema_text.encode("utf-8"),
-    )
-    ros2_schema, _ = Ros2MsgSchema().parse(schema)
-
-    field = ros2_schema.fields["name"]
-    assert isinstance(field, Primitive)
-    assert field.type == "string"
-    assert field.default == "John"
-
-
-def test_parse_default_string_value_single_quotes():
-    schema_text = "string nickname 'Johnny'\n"
-    schema = SchemaRecord(
-        id=1,
-        name="pkg/msg/StringDefaultSingle",
-        encoding="ros2msg",
-        data=schema_text.encode("utf-8"),
-    )
-    ros2_schema, _ = Ros2MsgSchema().parse(schema)
-
-    field = ros2_schema.fields["nickname"]
-    assert isinstance(field, Primitive)
-    assert field.type == "string"
-    assert field.default == "Johnny"
-
-
-def test_parse_default_string_value_with_hash():
-    schema_text = 'string tag "hello#world"\n'
-    schema = SchemaRecord(
-        id=1,
-        name="pkg/msg/StringWithHash",
-        encoding="ros2msg",
-        data=schema_text.encode("utf-8"),
-    )
-    ros2_schema, _ = Ros2MsgSchema().parse(schema)
-
-    field = ros2_schema.fields["tag"]
-    assert isinstance(field, Primitive)
-    assert field.type == "string"
-    assert field.default == "hello#world"
-
-
-def test_parse_default_array_of_ints():
-    schema_text = "int32[] samples [-200, -100, 0, 100, 200]\n"
-    schema = SchemaRecord(
-        id=1,
-        name="pkg/msg/ArrayDefault",
-        encoding="ros2msg",
-        data=schema_text.encode("utf-8"),
-    )
-    ros2_schema, _ = Ros2MsgSchema().parse(schema)
-
-    field = ros2_schema.fields["samples"]
-    assert isinstance(field, Sequence)
-    assert field.type == "int32"
-    assert field.default == [-200, -100, 0, 100, 200]
-
-
-def test_parse_constant_string_field_double_quotes():
-    schema_text = 'string GREETING="hello"\n'
-    schema = SchemaRecord(
-        id=1,
-        name="pkg/msg/StringConstDouble",
-        encoding="ros2msg",
-        data=schema_text.encode("utf-8"),
-    )
-    ros2_schema, _ = Ros2MsgSchema().parse(schema)
-
-    field = ros2_schema.fields["GREETING"]
-    assert isinstance(field, Constant)
-    assert field.type == "string"
-    assert field.value == "hello"
-
-
-def test_parse_constant_string_field_single_quotes():
-    schema_text = "string FAREWELL='bye'\n"
-    schema = SchemaRecord(
-        id=1,
-        name="pkg/msg/StringConstSingle",
-        encoding="ros2msg",
-        data=schema_text.encode("utf-8"),
-    )
-    ros2_schema, _ = Ros2MsgSchema().parse(schema)
-
-    field = ros2_schema.fields["FAREWELL"]
-    assert isinstance(field, Constant)
-    assert field.type == "string"
-    assert field.value == "bye"
+    assert point_schema.fields["z"].type == "float64"
 
 
 def test_constant_name_must_be_uppercase():
@@ -328,3 +374,15 @@ def test_field_with_inline_comment():
     field = ros2_schema.fields["value"]
     assert isinstance(field, Primitive)
     assert field.type == "int32"
+
+
+def test_invalid_field_name_raises_error():
+    schema_text = "int32 invalid__name\n"
+    schema = SchemaRecord(
+        id=1,
+        name="pkg/msg/InvalidName",
+        encoding="ros2msg",
+        data=schema_text.encode("utf-8"),
+    )
+    with pytest.raises(Ros2MsgError):
+        Ros2MsgSchema().parse(schema)
