9ffac00a8d7d41cf3a9d4101d04d8ee0475f09dc
Angus Stewart <angus.stewart@sensmore.ai>
2025-08-12 22:48:46 +0000

    fix: Import order

--- src/pybag/__init__.py
+++ src/pybag/__init__.py
@@ -1,17 +1,17 @@
 from .types import (
     Array,
     Complex,
+    bool,
     float32,
     float64,
     int8,
     int16,
     int32,
     int64,
-    bool,
     string,
-    wstring,
     uint8,
     uint16,
     uint32,
-    uint64
+    uint64,
+    wstring
 )
