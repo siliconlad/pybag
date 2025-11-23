51f0d4266d70b2e7e7d7deb50a20595e206ccbae
Angus Stewart <angus.stewart@sensmore.ai>
2025-11-23 19:25:16 +0100

    fix: Style

--- src/pybag/mcap/crc.py
+++ src/pybag/mcap/crc.py
@@ -16,14 +16,12 @@ class McapInvalidCrcError(Exception):
 
 
 def validate_crc(data: bytes, crc: int) -> bool:
-    """Validate CRC values of an MCAP file.
-    """
+    """Validate CRC values of an MCAP file."""
     return zlib.crc32(data) == crc
 
 
 def assert_crc(data: bytes, crc: int) -> None:
-    """Assert CRC values of an MCAP file.
-    """
+    """Assert CRC values of an MCAP file."""
     if not validate_crc(data, crc):
         raise McapInvalidCrcError(f'Invalid CRC for data')
 
